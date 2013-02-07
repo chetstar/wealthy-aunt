0e8f4857e8ca93306186d3e3f13584d07ee40e8e
Kellan Jacobs <kellankade@gmail.com>
2013-02-07 13:21:59 -0800

    Changed crack() to possible_passwords() to better reflect what the function does.

--- test_passwords.py
+++ test_passwords.py
@@ -1,13 +1,16 @@
+""" Test cases for our python password cracker """
 
 
-def crack(first, last, dob, date2, mothers_maiden, spouse, date3, pet1, pet2,
-          first_love, child1, child2):
+def possible_passwords(first, last, dob, date2, mothers_maiden, spouse, date3,
+                       pet1, pet2, first_love, child1, child2):
+    '''Function takes a list of possible data values and returns a list of
+    possible passwords'''
     pass
 
 
 def test_Isaiah():
-    result = crack("Isaiah", "Bailey", "03/29/1956", "03/27/1951", "Morgan",
-                   "Brody", "09/20/1954", "Sparky", "Mia", "Ava", "Serenity",
-                   "Nevaeh")
+    result = possible_passwords("Isaiah", "Bailey", "03/29/1956",
+                                "03/27/1951", "Morgan", "Brody", "09/20/1954",
+                                "Sparky", "Mia", "Ava", "Serenity", "Nevaeh")
 
     assert("Sparky" in result, "crack in not finding pet as password")
